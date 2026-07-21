b2683f39a7e9e6db66535bb363900ba3491a5821
Omar Biz <189382177+MrOBiz@users.noreply.github.com>
2026-07-21 08:46:12 +0200

    Added initial files and skeleton

---
+++ script.js
@@ -0,0 +1,20 @@
+ /*const book1 = new Book("A", "Sis", "123", "not read");
+        const book2 = new Book("B", "Bro", "321", "read");
+
+        function Book(title, author, pages, read){
+            if(!new.target){
+                throw Error("Use new!")
+            }
+
+            this.title = title;
+            this.author = author;
+            this.pages = pages;
+            this.read = read;
+
+            this.info = function(){
+                console.log(this.title + " by " + this.author +
+                            ", " + this.pages + ", " + this.read); 
+                return(this.title + " by " + this.author +
+                        ", " + this.pages + ", " + this.read);
+            }
+        } */
